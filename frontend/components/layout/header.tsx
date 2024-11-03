0dee9b8bb17683f715d7490f7cbaad071cb02125
Vishwajeet Ghatage <cloudmail.vishwajeet@gmail.com>
2024-11-03 14:29:04 +0530

    fe: add homepage

--- frontend/components/layout/header.tsx
+++ frontend/components/layout/header.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { ThemeToggle } from "../theme/theme-toggle";
+import Logo from "./logo";
 
 export default function Header() {
   return (
     <header className="top-0 w-full h-14">
       <nav className="container mx-auto h-full max-w-7xl px-4 flex items-center justify-between">
-        <Link href="/" className="text-2xl text-primary font-bold">
-          FormWise
-        </Link>
+        <Logo />
 
         <div className="md:flex items-center">
           <Button variant="outline">
---
+++ frontend/components/layout/logo.tsx
@@ -0,0 +1,12 @@
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { Icons } from "../icons";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="flex items-center space-x-2">
+      <Icons.logo className="h-6 w-6" />
+      <span className="text-2xl font-bold">{siteConfig.name}</span>
+    </Link>
+  );
+}
