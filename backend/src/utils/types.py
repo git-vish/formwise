23d0a5a84d3db01d85ad8b7eef79a20c2fa44651
Vishwajeet Ghatage <cloudmail.vishwajeet@gmail.com>
2024-11-15 08:19:21 +0530

    be: add form field models

--- backend/src/utils/types.py
+++ backend/src/utils/types.py
@@ -1,11 +1,7 @@
-import re
-from typing import Annotated, TypeVar
+from typing import Annotated
 
-from pydantic import AfterValidator, StringConstraints, UrlConstraints
+from pydantic import StringConstraints, UrlConstraints
 from pydantic_core import MultiHostUrl
-
-# Type variable for generic type hints
-T = TypeVar("T")
 
 # Custom types
 type MongoDsn = Annotated[MultiHostUrl, UrlConstraints(allowed_schemes=["mongodb+srv"])]
@@ -17,25 +13,3 @@ type NonEmptyStr = Annotated[
 type Label = Annotated[
     str, StringConstraints(min_length=1, max_length=100, strip_whitespace=True)
 ]
-
-
-def validate_regex(pattern: str) -> str:
-    """Validates if the given string is a valid regular expression.
-
-    Args:
-        pattern (str): The regular expression pattern to validate.
-
-    Returns:
-        str: The validated regular expression pattern.
-
-    Raises:
-        ValueError: If the pattern is not a valid regular expression.
-    """
-    try:
-        re.compile(pattern)
-        return pattern
-    except re.error as err:
-        raise ValueError(f"Invalid regular expression pattern: {err}") from err
-
-
-type Pattern = Annotated[str, AfterValidator(validate_regex)]
